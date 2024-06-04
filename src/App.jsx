8b1c25c523fc0c15d20c32f9c2f7b7319352ce35
the0therguy <nblues399@gmail.com>
2024-06-04 18:49:32 +0600

    form creation done.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
   const [content, setContent] = useState('');
   const [courseName, setCourseName] = useState('');
   const [overview, setOverview] = useState("")
+  const [duration, setDuration] = useState(0);
+  const [lectures, setLectures] = useState(0);
+  const [language, setLanguage] = useState("");
+  const [skillLevel, setSkillLevel] = useState("");
+  const [quizzes, setQuizzes] = useState(0);
+  const [access, setAccess] = useState("");
 
   const handleEditorData = (data) => {
     setContent(data)
@@ -16,15 +22,35 @@ function App() {
     setOverview(overViewData)
   }
 
+  const handleForm = () => {
+    console.log('data')
+  }
+
   return (
     <>
-      <form>
-        <input onChange={(e) => setCourseName(e.target.value)} value={courseName} type="text"/>
+      <form onSubmit={handleForm}>
+        <input onChange={(e) => setCourseName(e.target.value)} value={courseName} type="text" placeholder="Course Name" />
+        <br/>
+        <input onChange={(e) => setDuration(e.target.value)} value={duration} type="number" placeholder="Duration" />
+        <br/>
+        <input value={lectures} onChange={(e) => setLectures(e.target.value)} type="number" placeholder="Lectures"/>
+        <br/>
+        <input value={language} onChange={(e) => setLanguage(e.target.value)} type="text" placeholder="Language"/>
+        <br/>
+        <input value={skillLevel} onChange={(e) => setSkillLevel(e.target.value)} type="text" placeholder="Skill Level"/>
+        <br/>
+        <input value={quizzes} onChange={(e) => setQuizzes(e.target.value)} type="number" placeholder="Quizzes"/>
+        <br/>
+        <input value={access} onChange={(e) => setAccess(e.target.value)} type="text" placeholder="Access"/>
         <br/>
         <br/>
-        <CourseOverViewTextEditor data={overview} handleOverViewChange={handleCourseOverViewChange} />
+        <CourseOverViewTextEditor data={overview} handleOverViewChange={handleCourseOverViewChange}/>
         <br/>
-        <TextEditorComponent setData={handleEditorData}/>
+        <br/>
+        <TextEditorComponent data={content} setData={handleEditorData}/>
+        <br/>
+        <br/>
+        <button value="submit"> Submit </button>
       </form>
     </>
   )
--- src/Components/TextEditorComponent.jsx
+++ src/Components/TextEditorComponent.jsx
@@ -2,7 +2,7 @@ import { Editor } from '@tinymce/tinymce-react';
 import { useRef } from 'react';
 
 // eslint-disable-next-line react/prop-types
-export default function TextEditorComponent({setData}) {
+export default function TextEditorComponent({data, setData}) {
   const editorRef = useRef(null);
 
   const handleEditorChange = (content) => {
@@ -16,6 +16,7 @@ export default function TextEditorComponent({setData}) {
       <Editor
         apiKey='q9ik985j294lcwdzo5eop5uicv2d6stzfoc6cy9cfaeobass'
         onInit={(_evt, editor) => editorRef.current = editor}
+        initialValue={data}
         init={{
           plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
           toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
