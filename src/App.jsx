7ca9db2105200bc80ce30a8d7e1e7b40daa4f379
the0therguy <nblues399@gmail.com>
2024-06-05 18:19:11 +0600

    handle content

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import TextEditorComponent from "./Components/TextEditorComponent.jsx";
-import {useState} from "react";
+import {useState} from 'react';
 import CourseOverViewTextEditor from "./Components/CourseOverViewTextEditor.jsx";
 
 function App() {
@@ -22,24 +22,32 @@ function App() {
     setOverview(overViewData)
   }
 
-  const handleForm = () => {
+  const handleForm = (e) => {
+    e.preventDefault();
     console.log('data')
   }
 
   return (
     <>
       <form onSubmit={handleForm}>
-        <input onChange={(e) => setCourseName(e.target.value)} value={courseName} type="text" placeholder="Course Name" />
+        <input onChange={(e) => setCourseName(e.target.value)} value={courseName} type="text"
+               placeholder="Course Name"/>
         <br/>
-        <input onChange={(e) => setDuration(e.target.value)} value={duration} type="number" placeholder="Duration" />
+        <input
+          onChange={(e) => setDuration(parseInt(e.target.value))}
+          value={duration}
+          type="number"
+          placeholder="Duration"
+        />
         <br/>
-        <input value={lectures} onChange={(e) => setLectures(e.target.value)} type="number" placeholder="Lectures"/>
+        <input value={lectures} onChange={(e) => setLectures(parseInt(e.target.value))} type="number" placeholder="Lectures"/>
         <br/>
         <input value={language} onChange={(e) => setLanguage(e.target.value)} type="text" placeholder="Language"/>
         <br/>
-        <input value={skillLevel} onChange={(e) => setSkillLevel(e.target.value)} type="text" placeholder="Skill Level"/>
+        <input value={skillLevel} onChange={(e) => setSkillLevel(e.target.value)} type="text"
+               placeholder="Skill Level"/>
         <br/>
-        <input value={quizzes} onChange={(e) => setQuizzes(e.target.value)} type="number" placeholder="Quizzes"/>
+        <input value={quizzes} onChange={(e) => setQuizzes(parseInt(e.target.value))} type="number" placeholder="Quizzes"/>
         <br/>
         <input value={access} onChange={(e) => setAccess(e.target.value)} type="text" placeholder="Access"/>
         <br/>
@@ -50,7 +58,7 @@ function App() {
         <TextEditorComponent data={content} setData={handleEditorData}/>
         <br/>
         <br/>
-        <button value="submit"> Submit </button>
+        <button type="submit">Submit</button>
       </form>
     </>
   )
--- src/Components/TextEditorComponent.jsx
+++ src/Components/TextEditorComponent.jsx
@@ -1,15 +1,23 @@
-import { Editor } from '@tinymce/tinymce-react';
-import { useRef } from 'react';
+import {Editor} from '@tinymce/tinymce-react';
+import {useEffect, useRef, useState} from 'react';
 
 // eslint-disable-next-line react/prop-types
 export default function TextEditorComponent({data, setData}) {
   const editorRef = useRef(null);
 
-  const handleEditorChange = (content) => {
-    setData(content);
+  const [value, setValue] = useState(data ?? '');
+  useEffect(() => setValue(data ?? ''), [data]);
+  const handleEditorChange = (newValue, editor) => {
+    setValue(newValue);
+    editorRef.current = newValue; // Store the latest value in ref
   };
 
-
+  const handleSave = (e) => {
+    e.preventDefault()
+    if (editorRef.current) {
+      setData(editorRef.current);
+    }
+  };
 
   return (
     <>
@@ -17,12 +25,14 @@ export default function TextEditorComponent({data, setData}) {
         apiKey='q9ik985j294lcwdzo5eop5uicv2d6stzfoc6cy9cfaeobass'
         onInit={(_evt, editor) => editorRef.current = editor}
         initialValue={data}
+        value={value}
         init={{
           plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
           toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
         }}
         onEditorChange={handleEditorChange}
       />
+      <button onClick={handleSave}>Save</button>
     </>
   );
 }
